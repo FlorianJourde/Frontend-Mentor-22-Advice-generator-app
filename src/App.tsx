48d106e196c29b89b61f3382652e7412bbb418b8
Florian Jourde <jourdeflorian@gmail.com>
2024-02-27 00:39:13 +0100

    Finish challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import { useEffect, useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import desktopDivider from './assets/pattern-divider-desktop.svg'
 import mobileDivider from './assets/pattern-divider-mobile.svg'
 import dice from './assets/icon-dice.svg'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
   const [advice, setAdvice] = useState<string>('');
   const [adviceId, setAdviceId] = useState<number>(0);
 
   useEffect(() => {
     updateAdvice()
   }, []);
+
+  function handleClick(event: any) {
+    animateButton(event.currentTarget)
+    updateAdvice()
+  }
 
   function updateAdvice() {
     fetch('https://api.adviceslip.com/advice')
@@ -27,16 +29,27 @@ function App() {
       });
   }
 
+  function animateButton(clickedButton: any) {
+    clickedButton.classList.add('rotate');
+
+    setTimeout(function () {
+      clickedButton.classList.remove("rotate");
+    }, 2000);
+  }
+
   return (
     <>
-      <div className="card flex flex-col justify-center items-center bg-dark-blue p-8 gap-8 relative [&>*:nth-last-child(2)]:mb-8 rounded-xl shadow-2xl">
-        <h1 className='uppercase text-sm text-light-green text-bold'>Advice {adviceId}</h1>
-        <p className='text-very-light-blue text-xl'>"{advice}"</p>
-        <img src={desktopDivider} alt="" />
-        <a href="#" onClick={updateAdvice} className='absolute left-1/2 bottom-0 -translate-x-1/2 translate-y-1/2 bg-light-green p-5 aspect-square rounded-full'>
-          <img src={dice} alt="" />
-        </a>
-      </div >
+      <div className="wrapper md:max-w-[500px] max-w-[400px]">
+        <div className="card flex flex-col justify-center items-center bg-dark-blue p-8 gap-8 relative rounded-xl shadow-2xl w-full mt-5 mb-12">
+          <h1 className='uppercase text-xs text-light-green text-bold tracking-[.25em] font-bold'>Advice #{adviceId}</h1>
+          <p className='text-very-light-blue text-xl font-bold text-center'>"{advice}"</p>
+          <img className='hidden md:block mb-10' src={desktopDivider} alt="Desktop divider" />
+          <img className='md:hidden mb-10' src={mobileDivider} alt="Desktop divider" />
+          <button onClick={event => handleClick(event)} className='absolute left-1/2 bottom-0 -translate-x-1/2 translate-y-1/2 bg-light-green p-5 aspect-square rounded-full transition-shadow duration-500 hover:shadow-[0_0_20px_15px_#6efcac40]'>
+            <img src={dice} alt="" />
+          </button>
+        </div >
+      </div>
     </>
   )
 }
